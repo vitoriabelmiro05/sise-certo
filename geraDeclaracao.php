712ca0b1a4ee4dc14c67353a644d13191c3f3a91
Igor Lamoia Queiroz <62469164+igorlamoia@users.noreply.github.com>
2021-04-07 22:42:41 -0300

    ajustando pdf

--- geraDeclaracao.php
+++ geraDeclaracao.php
@@ -1,53 +1,70 @@
 <?php
 include('fpdf/fpdf.php');
+include('conexao.php');
 // ini_set('display_errors', 1);
 // ini_set('display_startup_erros', 1);
 // error_reporting(E_ALL); 
 
 // require_once("config.php");
 
+session_start();
+//include('verifica_login.php');
+include('conexao.php');
+
+$consulta= "SELECT * FROM usuario WHERE cpf = '$_SESSION[CPF]'; ";
+$consultaeS= "SELECT * FROM estagio; ";
+$con= mysqli_query($conn, $consulta);
+$con2= mysqli_query($conn, $consulta);
+$con3= mysqli_query($conn, $consultaeS);
+
+$dado = $con3 -> fetch_array();
+
+
 class MYPDF extends FPDF
 {
 
 }
 $pdf = new MYPDF('P'); //Colocaï¿½ï¿½o da pï¿½gina. P => normal e L => paisagem
-
-
-
 $pdf->AddPage();
 
-$pdf->Image('imagens/LOGO.png', 10, 5, 35, 20, 'PNG');
+//x, y, largura, altura
+$pdf->Image('imagens/brasao.png', 95, 10 , 20, 20, 'PNG');
 
-$pdf->SetY(25);
-$pdf->SetX(10);
-$pdf->SetFont('Times', '', 14);
+$pdf->SetY(35);
+$pdf->SetX(30);
+$pdf->SetFont('Arial', 'B', 10);
 $w = array(90);
-$pdf->Cell(0, 2, 'FICHA CADASTRAL', 0, 1, 'L', 0);
-$pdf->ln();
-$pdf->ln();
-$pdf->SetFont('Times', 'B', 16);
-$pdf->SetX(10);
+$pdf->Cell(0, 5, 'SERVIÇO PÚBLICO FEDERAL', 0, 1, 'C', 0);
+$pdf->Cell(0, 5, 'MINISTÉRIO DA EDUCAÇÃO', 0, 1, 'C', 0);
+$pdf->Cell(0, 5, 'CENTRO FEDERAL DE EDUCAÇÃO TECNOLÓGICA DE MINAS GERAIS', 0, 1, 'C', 0);
+$pdf->Cell(0, 5, 'CAMPUS LEOPOLDINA', 0, 1, 'C', 0);
+$pdf->Cell(0, 5, 'COORDENAÇÃO DE PROGRAMAS DE ESTÁGIO', 0, 1, 'C', 0);
+
+$pdf->SetFont('Arial', 'B', 16);
+$pdf->SetX(30);
 $w = array(90);
-$pdf->Cell(0, 2, 'PESSOA Fï¿½SICA', 0, 1, 'L', 0);
 $pdf->ln();
 $pdf->ln();
+$pdf->Cell(0, 5, 'DECLACAÇÃO Nº 25/2020', 0, 1, 'C', 0);
+$pdf->SetFont('Arial', '', 12);
+$pdf->ln();
+$pdf->SetX(30);
+$pdf->MultiCell(150, 5, 'Declaramos para os devidos fins que o professor(a) ' . $dado["nome_orientador"] . ' orientou os alunos abaixo relacionados em seus respectivos estágios:', 0, 'J', 0);
 
 $pdf->ln();
 $pdf->ln();
 $pdf->ln();
-$pdf->ln();
-$pdf->ln();
 
-$pdf->SetFont('Times', 'B', 12);
+$pdf->SetFont('Arial', 'B', 12);
 $pdf->SetFillColor(192,192,192);
 $w = array(37, 58, 58, 37);
 $pdf->Cell($w[0], 5, 'Uso exclusivo ', 'TLR', 0, 'C', 1);
 $pdf->Cell($w[1], 5, '', 'LTR', 0, 'L', 1);
-$pdf->SetFont('Times', '', 8);
+$pdf->SetFont('Arial', '', 8);
 $pdf->Cell($w[2], 5, 'Tipo de Cliente ', 'TLR', 0, 'R', 1);
 $pdf->Cell($w[3], 5, 'Cï¿½digo do Cliente', 'LTR', 1, 'R', 1);
 
-$pdf->SetFont('Times', '', 10);
+$pdf->SetFont('Arial', '', 10);
 $pdf->SetFillColor(192,192,192);
 $w = array(37, 58, 58, 37);
 $pdf->Cell($w[0], 3, '', 'BLR', 0, 'L', 1);
@@ -57,27 +74,27 @@ $pdf->Cell($w[3], 3, '', 'LBR', 1, 'L', 1);
 $pdf->Ln();
 $pdf->Ln();
 
-$pdf->SetFont('Times', 'B', 11);
+$pdf->SetFont('Arial', 'B', 11);
 $w = array(190);
 $pdf->Cell($w[0], 5, utf8_encode('DADOS PESSOAIS '), 0, 1, 'L', 0);
 $w = array(140,50);
-$pdf->SetFont('Times', '', 8);
+$pdf->SetFont('Arial', '', 8);
 $pdf->Cell($w[0], 5, 'Nome Completo:', 'TLR', 0, 'L', 0);
 $pdf->Cell($w[1], 5, 'CPF:', 'TR', 1, 'L', 0);
 
 $w = array(95,95);
-$pdf->SetFont('Times', '', 8);
+$pdf->SetFont('Arial', '', 8);
 $pdf->Cell($w[0], 5, 'Nome do pai:', 'TLR', 0, 'L', 0);
 $pdf->Cell($w[1], 5, 'Nome do mï¿½e:', 'TR', 1, 'L', 0);
 
 $w = array(45,50,95);
-$pdf->SetFont('Times', '', 8);
+$pdf->SetFont('Arial', '', 8);
 $pdf->Cell($w[0], 5, 'Data de Nascimento:', 'TLR', 0, 'L', 0);
 $pdf->Cell($w[1], 5, 'Nacionalidade:', 'TR', 0, 'L', 0);
 $pdf->Cell($w[2], 5, 'Naturalidade: (municï¿½pio)', 'TR', 1, 'L', 0);
 
 $w = array(45,50,95);
-$pdf->SetFont('Times', '', 8);
+$pdf->SetFont('Arial', '', 8);
 $pdf->Cell($w[0], 5, 'Sexo:', 'TLR', 0, 'L', 0);
 $pdf->Cell($w[1], 5, 'Estado Civil:', 'TR', 0, 'L', 0);
 $pdf->Cell($w[2], 5, 'Nome do cï¿½njuge ou companheiro(a): Avalista', 'TR', 1, 'L', 0);
@@ -88,17 +105,18 @@ $pdf->Cell($w[1], 2, '', 'R', 0, 'L', 0);
 $pdf->Cell($w[2], 2, '', 'R', 1, 'L', 0);
 
 $w = array(3,5,16,5,16,50,95);
-$pdf->SetFont('Times', '', 8);
+$pdf->SetFont('Arial', '', 8);
 $pdf->Cell($w[0], 5, '','L', 0, 'L', 0);
-$pdf->SetFont('Times', '', 15);
-$pdf->SetFont('Times', '', 8);
+$pdf->SetFont('Arial', '', 15);
+$pdf->SetFont('Arial', '', 8);
 $pdf->Cell($w[2], 5,  ' Masculino', 0, 0, 'L', 0);
-$pdf->SetFont('Times', '', 15);
-$pdf->SetFont('Times', '', 8);
+$pdf->SetFont('Arial', '', 15);
+$pdf->SetFont('Arial', '', 8);
 $pdf->Cell($w[4], 5,  ' Feminino', 0, 0, 'L', 0);
 
 $pdf->Ln();
 $pdf->MultiCell(190, 5, 'lorem aoijdjadiosjidsaijdsajiadjijadijasdijdsaidsajidsjaisadjisajiasdjisajdiasjdidasjdiasjdasdasijdaisjdiasjsadidsjaidsajiojidasjdasSe eu quebro linha aqui, quebra linha no pdf Primeiro Largura, depois altura (no MultiCell tanto faz depois o que tem q escrever, se vai colocar bora,texto alinhado L esquerda, C centralizado, J justificado, e no fim bota 0 pra n ter cor', 1, 'C', 0);
+
 
 $pdf->Output('fichaCadastral' . '.pdf', 'I'); //I exibe na prï¿½pria aba e 'D', forï¿½a um download
 
