0ec0752fbb8926fc3b773a24ab5270d32220c1cc
vitoriabelmiro05 <vitorianapo9@gmail.com>
2021-04-21 17:13:58 -0300

    alert

--- incaPO2.php
+++ incaPO2.php
@@ -13,7 +13,9 @@ $nome_orientador = filter_input(INPUT_POST, 'NOMEO', FILTER_SANITIZE_EMAIL);
     // $query2= mysqli_query($conn, "update estagio inner join usuario on usuario.nome = '$nome_orientador' set cpf_usuario = cpf where idestagio = '$idestagio';");
 
      if($query){
-         header("Location: verifica_usuario.php");
+        echo "<script> alert('Indicação enviada com  sucesso!');";
+        echo "javascript:window.location='verifica_usuario.php';</script>";
+        
      } else{
         die("Erro: ". mysqli_error($query));
      }
