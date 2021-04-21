49ccf121b85e9d784d0896f9af153c3853ddee05
vitoriabelmiro05 <vitorianapo9@gmail.com>
2021-04-21 11:45:27 -0300

    mudei

--- aprovaPO2.php
+++ aprovaPO2.php
@@ -7,7 +7,10 @@ include('conexao.php');
 $idestagio = filter_input(INPUT_POST, 'idestagio', FILTER_SANITIZE_STRING);
 $nome_orientador = filter_input(INPUT_POST, 'NOMEO', FILTER_SANITIZE_EMAIL);
 
-    $query = mysqli_query($conn, "update estagio set nome_orientador = '$nome_orientador', aprovacao= '1' where idestagio = '$idestagio';" );
+$sql= mysqli_query($conn, "SELECT cpf from usuario where nome = '$nome_orientador';");
+$cpf_prof= mysqli_fetch_row($sql);
+
+    $query = mysqli_query($conn, "update estagio set nome_orientador = '$nome_orientador', aprovacao= '1', cpf_usuario = '$cpf_prof' where idestagio = '$idestagio';" );
 
      if($query){
          header("Location: verifica_usuario.php");
--- incaPO2.php
+++ incaPO2.php
@@ -7,7 +7,10 @@ include('conexao.php');
 $idestagio = filter_input(INPUT_POST, 'idestagio', FILTER_SANITIZE_STRING);
 $nome_orientador = filter_input(INPUT_POST, 'NOMEO', FILTER_SANITIZE_EMAIL);
 
+
+
     $query = mysqli_query($conn, "update estagio set nome_orientador = '$nome_orientador' where idestagio = '$idestagio';" );
+    // $query2= mysqli_query($conn, "update estagio inner join usuario on usuario.nome = '$nome_orientador' set cpf_usuario = cpf where idestagio = '$idestagio';");
 
      if($query){
          header("Location: verifica_usuario.php");
